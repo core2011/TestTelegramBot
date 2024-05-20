e829f45e69598d34349d989f094e3ae8c57c5445
core2011 <56308687+core2011@users.noreply.github.com>
2024-05-20 16:51:09 +0300

    add db it works

---
+++ src/main/java/com/mybottest/ApplicationTelegramBot.java
@@ -0,0 +1,11 @@
+package com.mybottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTelegramBot {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTelegramBot.class);
+    }
+}
